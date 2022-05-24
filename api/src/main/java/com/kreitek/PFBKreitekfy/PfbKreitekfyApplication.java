0ed5732830a4ba47ed46ba805edaefa8821011aa
Alberto León <37343812+Albeleon@users.noreply.github.com>
2022-05-24 11:18:02 +0200

    0.1 - Inicializacion del proyecto

---
+++ api/src/main/java/com/kreitek/PFBKreitekfy/PfbKreitekfyApplication.java
@@ -0,0 +1,13 @@
+package com.kreitek.PFBKreitekfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfbKreitekfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfbKreitekfyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/kreitek/PFBKreitekfy/PfbKreitekfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreitek.PFBKreitekfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfbKreitekfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
