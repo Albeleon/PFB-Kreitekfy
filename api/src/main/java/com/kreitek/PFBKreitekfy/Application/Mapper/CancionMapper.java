015aa56f564076b84f6c06733cc7d7ac625650a4
Nacho Pedros Gil <nacho.pedrosgil@outlook.com>
2022-05-25 12:39:15 +0200

    update pom.xml mapstruct dependency

--- api/src/main/java/com/kreitek/PFBKreitekfy/Application/Mapper/CancionMapper.java
+++ api/src/main/java/com/kreitek/PFBKreitekfy/Application/Mapper/CancionMapper.java
@@ -1,5 +1,8 @@
 package com.kreitek.PFBKreitekfy.Application.Mapper;
 
 
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CancionMapper extends EntityMapper {
 }
