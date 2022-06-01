13b9d689a0e60bada6423c9d2c0b1434a865b2d9
Nacho <nacho.pedrosgil@outlook.com>
2022-06-01 12:59:41 +0200

    proteccion AuthGuard para todas las rutas

--- site/src/app/backoffice/backoffice-routing.module.ts
+++ site/src/app/backoffice/backoffice-routing.module.ts
@@ -5,17 +5,24 @@ import { TablaCancionesComponent } from './componentes/tabla-canciones/tabla-can
 import { TablaAlbumesComponent } from './componentes/tabla-albumes/tabla-albumes.component';
 import { TablaArtistasComponent } from './componentes/tabla-artistas/tabla-artistas.component';
 import { TablaEstilosComponent } from './componentes/tabla-estilos/tabla-estilos.component';
+import { AdminGuard } from '../guards/admin.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'canciones', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'canciones',
+    pathMatch: 'full',
+    canActivate: [AdminGuard],
+  },
   {
     path: '',
     component: BackofficeComponent,
+    canActivate: [AdminGuard],
     children: [
       { path: 'canciones', component: TablaCancionesComponent },
       { path: 'albumes', component: TablaAlbumesComponent },
       { path: 'artistas', component: TablaArtistasComponent },
-      { path: 'estilos', component: TablaEstilosComponent }
+      { path: 'estilos', component: TablaEstilosComponent },
     ],
   },
 ];
--- site/src/app/player/player-routing.module.ts
+++ site/src/app/player/player-routing.module.ts
@@ -1,21 +1,27 @@
 import { Input, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserGuard } from '../guards/user.guard';
 import { Estilo } from '../models/estilo.interface';
 import { CancionDataComponent } from './componentes/cancion-data/cancion-data.component';
 import { PaginaPrincipalComponent } from './componentes/pagina-principal/pagina-principal.component';
 import { PlayerComponent } from './player.component';
 
 const routes: Routes = [
-  { path: '', component: PlayerComponent, children: [
-    { path: '', component: PaginaPrincipalComponent },
-    { path: 'fichaCancion/:cancionId', component: CancionDataComponent }
-  ] },
+  {
+    path: '',
+    component: PlayerComponent,
+    canActivate: [UserGuard],
+    children: [
+      { path: '', component: PaginaPrincipalComponent },
+      { path: 'fichaCancion/:cancionId', component: CancionDataComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class PlayerRoutingModule {
   @Input() estilo?: Estilo;
- }
+}
